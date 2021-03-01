421881216b719338987d3df81984b972faf6534f
leah-handel <leah.handel@gmail.com>
2021-02-28 20:59:04 -0600

    precipitation, station list, and temperatures in flask

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+import datetime as dt
+import numpy as np
+
 from flask import Flask, jsonify
 
 ######################################
@@ -25,24 +28,51 @@ def home():
     return(
         "Welcome to my Hawaii weather API!<br><br>"
         "Available Routes:<br><br>"
-        "/api/v1.0/precipitation<br>"
-        "/api/v1.0/stations<br>"
-        "/api/v1.0/tobs<br>"
-        "/api/v1.0/<start><br>"
-        "/api/v1.0/<start>/<end><br>"
+        "<strong>/api/v1.0/precipitation:</strong> The last year of precipitation measurements in the dataset<br>"
+        "<strong>/api/v1.0/stations:</strong> A list of stations in the dataset<br>"
+        "<strong>/api/v1.0/tobs:</strong><br> The last year of temperatures recorded at station USC00519281"
+        "/api/v1.0/<start_date><br>"
+        "/api/v1.0/<start_date>/<end_date><br>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    pass
+    session = Session(engine)
+    start_date = datetime.datetime(2016, 8, 23, 0, 0)
+    year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > start_date).all()
+    session.close()
+
+    results={}
+    for y in year:
+        date = y.date
+        precipitation=y.prcp
+        results["date"].append(precipitation)
+    
+    
+
 
 @app.route("/api/v1.0/stations")
 def stations():
-    pass
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
+
+    station_names=np.ravel(results)
+    return jsonify(station_names)
+
 
 @app.route("/api/v1.0/tobs")
 def temperature():
-    pass
+    session = Session(engine)
+    station = 'USC00519281'
+    start_date = datetime.datetime(2016, 8, 23, 0, 0)
+    results = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station==station).\
+    filter(Measurement.date > start_date).all()
+    session.close()
+
+    active_temps=np.ravel(results)
+    return jsonify(active_temps)
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
